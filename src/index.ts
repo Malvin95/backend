8e9fa94f8ed96d9d8f25b7981fb94eabe4360e3c
Malvin95 <Malvinharding@hotmail.co.uk>
2026-06-17 23:51:19 +0100

    CORS implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import createError from "http-errors";
+import * as cors from "cors";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -15,7 +16,30 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Environment-specific origins
+const allowedOrigins = [
+  'http://localhost:3000',  // Development
+  'http://localhost:5173'
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    // Allow requests with no origin (mobile apps, etc.)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'), false);
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
 app.use(logger("dev"));
+app.use(cors.default(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
